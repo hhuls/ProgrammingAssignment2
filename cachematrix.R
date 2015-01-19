98410a7729450daa5951fab0bab82177e253f384
hhuls <hhuls@hotmail.com>
2015-01-19 09:28:30 +0100

    ProgrAss2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y            ## caches inputted matrix for cacheSolve
+    m <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setinv <- function(i) {
+    inv <<- i
+  }
+  getinv <- function() {
+    m
+  }
+  ## make a list to house the four functions
+  list(set = set, get = get,
+       setinv = setinv, getinv = getinv)
 
 }
 
@@ -11,5 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+    
+  inv <- x$getinv() 
+  if(!is.null(inv)){ message("getting cached data") 
+                     return(inv) 
+  } 
+  
+  data <- x$get()         ## run getmatrix to get value of input matrix
+  inv <- solve(data, ...) ## compute value of inverse of input matrix
+  x$setinv(inv)           ## run setinverse to cache inverse
+  inv                     ## return the inverse
 }
